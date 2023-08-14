98138bd835be28168e57689b79ad9f86585d5c85
up201707219 <up201707219@fe.up.pt>
2023-08-14 02:07:15 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<main>
+  <div class="container">
+    <div class="child">
+      child 1
+      <p>t</p>
+      <p>e</p>
+      <p>s</p>
+      <p>t</p>
+      <p>e</p>
+    </div>
+    <div class="child">
+      child 2
+    </div>
+  </div>
+</main>
