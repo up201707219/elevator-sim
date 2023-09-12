7f1064049cf4fa2489811c8404b910a653098007
up201707219 <up201707219@g.uporto.pt>
2023-09-12 15:35:13 +0100

    favicon change

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,9 +1,19 @@
 
 <main>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eveniet voluptate perspiciatis reiciendis dolorum cum accusantium laudantium porro, ea aliquam voluptatum, minima voluptatibus quod maxime consequuntur numquam totam maiores laborum?
+    <div class="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eveniet voluptate perspiciatis reiciendis dolorum cum accusantium laudantium porro, ea aliquam voluptatum, minima voluptatibus quod maxime consequuntur numquam totam maiores laborum?
+    </div>
+    
 </main>
 
 <style>
-
+    .container{
+        width: 80%;
+        margin: 2rem;
+        margin-left: 10%;
+        margin-right: 10%;
+        margin-top: 8rem;
+        font-size: 14pt;
+    }
 </style>
 
