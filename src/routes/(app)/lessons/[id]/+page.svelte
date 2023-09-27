45e4e4eea08bf16e13f59a3f6d25b3d5342dff88
up201707219 <up201707219@g.uporto.pt>
2023-09-27 14:51:18 +0100

    lessons/[id] layout done

--- src/routes/(app)/lessons/[id]/+page.svelte
+++ src/routes/(app)/lessons/[id]/+page.svelte
@@ -1,32 +1,68 @@
 <script>
     export let data;
+    function stringify(min, max){
+        let timeType = "min";
+        if(min > 120){
+            [min, max] = [(min/60), (max/60)];
+            timeType = "h";
+        }
+        let str;
+        if(min === max){
+            str = min+timeType;
+        }
+        else{
+            str = min + "-" + max+timeType;
+        }
+        return str;
+    }
+    let duration = stringify(data.dur_min, data.dur_max);
+    let mobileDisplay = "lessons";
+
+    function updateMobileDisplay(display){
+        mobileDisplay = display;
+    }
 </script>
 
 <main>
-    <div class="course-details">
+    <div class="mobile-nav">
+        {#if mobileDisplay === "lessons"}
+            <button on:click={() => updateMobileDisplay("details")}>Detalhes</button>
+        {:else}
+            <button on:click={() => updateMobileDisplay("lessons")}>Módulos</button>
+        {/if}
+    </div>
+    <div class="course-details {(mobileDisplay === "details")? "open":""}">
         <h1>{data.name}</h1>
         <p><b>Descrição:</b> {@html data.description}</p>
-        <p><b>Duração:</b> {data.dur_min}-{data.dur_max}</p>
+        <p><b>Duração:</b> {duration}</p>
     </div>
-    {#each data.lessons as lesson, i}
-    <a href="/in_construction">
-        <div class="lesson">
-            <span class="lesson-content">Módulo {i+1}: {lesson}</span>
-        </div>    
-    </a>
-    {/each}
-    {#if data.lessons.length === 0}
+    <div class="lessons {(mobileDisplay === "lessons")? "open":""}">    
+        {#each data.lessons as lesson, i}
+        <a href="/in_construction">
+            <div class="lesson">
+                <span class="lesson-content">Módulo {i+1}: {lesson}</span>
+            </div>    
+        </a>
+        {/each}
+    </div>
+        {#if data.lessons.length === 0}
         <h2>Não existem lições para este módulo</h2>
     {/if}
 </main>
 
 <style>
+    .mobile-nav{
+        display: none;
+        width: 80%;
+        margin: auto;
+        margin-top: 2rem;
+    }
     .course-details{
         margin-left: auto;
         margin-right: auto;
         width: 80%;
         text-align: start;
-        margin-top: 3rem;
+        margin-top: 1rem;
         margin-bottom: 4rem;
     }
 
@@ -50,5 +86,23 @@
     a{
         text-decoration: none;
         color: black;
+    }
+
+    @media (max-width: 900px){
+        .mobile-nav{
+            display: flex;
+        }
+        .lessons{
+            display: none;
+        }
+        .lessons.open{
+            display: block;
+        }
+        .course-details{
+            display: none;
+        }
+        .course-details.open{
+            display: block;
+        }
     }
 </style>
