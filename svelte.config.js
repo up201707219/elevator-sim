a3406ede529db36166798332738d1f646f035024
up201707219 <up201707219@g.uporto.pt>
2023-10-19 10:34:34 +0100

    using adapter-node for building

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,37 @@
 // import adapter from '@sveltejs/adapter-auto';
 
+import adapter from '@sveltejs/adapter-node';
+
 // /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	}
-// };
 
-// export default config;
-
-
-import adapter from '@sveltejs/adapter-static';
-
-export default {
+const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: false,
+		  }
 	}
 };
+
+export default config;
+
+
+
+// import adapter from '@sveltejs/adapter-static';
+
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			// default options are shown. On some platforms
+// 			// these options are set automatically — see below
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: undefined,
+// 			precompress: false,
+// 			strict: true
+// 		})
+// 	}
+// };
